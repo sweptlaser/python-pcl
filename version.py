db908288f0c23390ac0f0e254f08de1ef23634f1
Erich E. Hoover <ehoover@sweptlaser.com>
2019-09-19 15:19:58 -0600

    Remove special characters from the version "string"
    This extra bit confuses pip for some strange reason.

--- version.py
+++ version.py
@@ -7,7 +7,7 @@ __timestamp__ = check_output('git log --no-walk --date=iso --format=%cd || true'
 __commit__ = check_output('git rev-parse --short HEAD || true', shell=True).decode("utf-8").rstrip()
 __extra_build__ = ''
 if __debug_build__ and __timestamp__:
-    __timestamp__ = parser.parse(__timestamp__).strftime("%Y%m%d+%H%M%S")
-    __extra_build__ = '+git~{}~{}'.format(__timestamp__, __commit__)
+    __timestamp__ = parser.parse(__timestamp__).strftime("%Y%m%d%H%M%S")
+    __extra_build__ = '-{}{}'.format(__timestamp__, __commit__)
 
 VERSION = '{}{}'.format(__major_version__, __extra_build__)
