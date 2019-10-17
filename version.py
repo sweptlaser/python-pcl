896d92e62c042c5ae395679b82fd03a040468e6e
Erich E. Hoover <ehoover@sweptlaser.com>
2019-10-17 15:47:39 -0600

    use '.dev' instead of '-' in the version string so that pip will do proper versioning

--- version.py
+++ version.py
@@ -8,6 +8,6 @@ __commit__ = check_output('git rev-parse --short HEAD || true', shell=True).deco
 __extra_build__ = ''
 if __debug_build__ and __timestamp__:
     __timestamp__ = parser.parse(__timestamp__).strftime("%Y%m%d%H%M%S")
-    __extra_build__ = '-{}{}'.format(__timestamp__, __commit__)
+    __extra_build__ = '.dev{}{}'.format(__timestamp__, __commit__)
 
 VERSION = '{}{}'.format(__major_version__, __extra_build__)
